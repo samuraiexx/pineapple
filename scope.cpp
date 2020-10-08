c0cbff0eb8ae27b1f2ea6656b42a8a51c6c1e7f5
Mateus de Castro <samuraiexx@gmail.com>
2020-10-08 16:47:03 -0300

    Add Scope file

---
+++ scope.cpp
@@ -0,0 +1,64 @@
+#include"scope.h"
+
+const int MAX_NEST_LEVEL = 100;
+
+Scope::Object* SymbolTable[MAX_NEST_LEVEL];
+Scope::Object* SymbolTableLast[MAX_NEST_LEVEL];
+int nCurrentLevel = -1;
+
+namespace Scope {
+  int newBlock() {
+    SymbolTable[++nCurrentLevel] = nullptr;
+    SymbolTableLast[nCurrentLevel] = nullptr;
+    return nCurrentLevel;
+  }
+
+  int endBlock() {
+    return --nCurrentLevel;
+  }
+
+  Object* define(int aName) {
+    Object* obj = new Object;
+    obj->nName = aName;
+    obj->pNext = nullptr;
+    if (SymbolTable[nCurrentLevel] == nullptr) {
+      SymbolTable[nCurrentLevel] = obj;
+      SymbolTableLast[nCurrentLevel] = obj;
+    } else {
+      SymbolTableLast[nCurrentLevel]->pNext = obj;
+      SymbolTableLast[nCurrentLevel] = obj;
+    }
+    return obj;
+  }
+
+  Object* search(int aName) {
+    Object* obj = SymbolTable[nCurrentLevel];
+    while (obj != nullptr) {
+      if (obj->nName == aName) {
+        break;
+      } else {
+        obj = obj->pNext;
+      }
+    }
+    return obj;
+  }
+
+  Object* find(int aName) {
+    Object *obj = nullptr;
+    for (int i = nCurrentLevel; i >= 0 && obj == nullptr; --i) {
+      obj = SymbolTable[i];
+      while (obj != nullptr) {
+        if (obj->nName == aName) {
+          break;
+        } else {
+          obj = obj->pNext;
+        }
+      }
+      if (obj != nullptr) {
+        break;
+      }
+    }
+    return obj;
+  }
+};
+
---
+++ scope.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Scope {
+  struct Object {
+    int nName;
+    Object* pNext;
+  };
+
+  int newBlock(void);
+  int endBlock(void);
+  Object* define(int aName);
+  Object* search(int aName);
+  Object* find(int aName);
+};
+
